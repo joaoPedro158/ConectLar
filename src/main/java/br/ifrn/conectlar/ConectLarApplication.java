174165da83e5d6e19baf85ca295c2b27ec0b9ffb
J0a0p3dr067 <pjpedro158@gmail.com>
2025-10-20 09:03:27 -0300

    inicio do proejto

---
+++ src/main/java/br/ifrn/conectlar/ConectLarApplication.java
@@ -0,0 +1,13 @@
+package br.ifrn.conectlar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectLarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConectLarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/ifrn/conectlar/ConectLarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifrn.conectlar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectLarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
