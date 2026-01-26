d628f72479ea4b149ac554acbd0d791ec2cc065a
Carlos Eduardo <carlos.vitor@escolar.ifrn.edu.br>
2026-01-26 19:33:34 -0300

    lapidando o html e desgrafmentando o css e o js

---
+++ src/main/resources/static/js/utilidades.js
@@ -0,0 +1,7 @@
+// Funções utilitárias e helpers
+function abrirModal() {
+    document.getElementById('modalNovoPedido').classList.remove('escondido');
+}
+function fecharModal() {
+    document.getElementById('modalNovoPedido').classList.add('escondido');
+}
